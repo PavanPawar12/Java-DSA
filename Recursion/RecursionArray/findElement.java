a940cb9f2600dde1df64a3e8106f79f30f644ca6
PavanPawar12 <pavanpawar9923100@gmail.com>
2026-01-08 22:46:13 +0530

    return index of element

--- Recursion/RecursionArray/findElement.java
+++ Recursion/RecursionArray/findElement.java
@@ -2,6 +2,11 @@ public class findElement {
     public static void main(String[] args) {
         int[] arr = {3,2,1,18,9}; // if 18 --> true , if 8 --> false
         System.out.println(find(arr, 8, 0));
+
+        // return index 
+        System.out.println(findIndex(arr, 18, 0));
+
+        
     }
     static boolean find(int[] arr, int target, int index){
         if(index == arr.length){
@@ -10,4 +15,18 @@ public class findElement {
 
         return arr[index] == target || find(arr, target, index+1);
     }
+    static int findIndex(int[] arr, int target, int index){
+        if(index == arr.length){
+            return -1;
+        }
+
+        if(arr[index] == target){
+            return index;
+        }else{
+
+            return findIndex(arr, target, index+1);
+        }
+        
+    }
+    
 }
