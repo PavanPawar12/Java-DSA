470ad471612414797d74f4efb00427167f7d3968
PavanPawar12 <pavanpawar9923100@gmail.com>
2026-01-11 21:45:21 +0530

    patter using recursion

--- Recursion/Patter_with_Recursion/Pattern.java
+++ Recursion/Patter_with_Recursion/Pattern.java
@@ -1,18 +1,18 @@
 
 public class Pattern {
     public static void main(String[] args){
-      printPattern(4, 0);
+      triangle(4, 0);
     }
-    static void printPattern(int r, int c){
+    static void triangle(int r, int c){
         if(r == 0){
             return;
         }
         if(c < r){
             System.out.print("*");
-            printPattern(r, c + 1);
+            triangle(r, c + 1);
         }else{
             System.out.println();
-            printPattern(r - 1, 0);
+            triangle(r - 1, 0);
         }
     }
 }
