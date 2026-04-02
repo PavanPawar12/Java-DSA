7c9ec7dbf4f05f298dbf239db6f672a2c31c5e09
PavanPawar12 <pavanpawar9923100@gmail.com>
2026-04-02 18:19:19 +0530

    Basic about tree data structure

--- Tree/BasicTree.java
+++ Tree/BasicTree.java
@@ -1,19 +1,48 @@
-class Node{
-    int data;
-    Node left, right;
-    Node(int data){
-        this.data = data;
+class TreeNode{
+    int val;
+    int count;
+    TreeNode left, right;
+
+    
+    public TreeNode(int val) {
+        this.val = val;
         this.left = null;
         this.right = null;
-    }
+    } 
 }
 
-public class BasicTree {
+public class BasicTree{
+    public int count;
+    BasicTree(){
+        this.count = 0;
+    }
+    public static void inorder(TreeNode root){
+        if(root == null) return;
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        
+        inorder(root.right);
+    }
+
+    public static int countNode(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        // recursive call 
+        int leftCount = countNode(root.left);
+        int rigtCount = countNode(root.right);
+        return 1+ leftCount + rigtCount;
+    }
     public static void main(String[] args){
-        Node root = new Node(1);
-        root.left = new Node(2);
-        root.right = new Node(3);
-        root.left.left = new Node(5);
-        root.left.right = new Node(9);
+        TreeNode tree = new TreeNode(99);
+        tree.left = new TreeNode(13); 
+        tree.left.left = new TreeNode(13); 
+        tree.right = new TreeNode(30); 
+        tree.right.right = new TreeNode(90); 
+        // inorder(tree);
+        System.out.println(countNode(tree));
+        // System.out.println(tree.val);
+        // System.out.println(tree.left.val);
+        // System.out.println(tree.right.val);
     }
 }
