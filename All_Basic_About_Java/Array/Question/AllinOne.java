908b09031de8debb53f7a022e5a58e657d5adc8f
PavanPawar12 <pavanpawar9923100@gmail.com>
2026-04-06 19:25:49 +0530

    commit change

--- All_Basic_About_Java/Array/Question/AllinOne.java
+++ All_Basic_About_Java/Array/Question/AllinOne.java
@@ -172,6 +172,7 @@ public class AllinOne {
         // for (int i = 0; i < arr.length; i++) {
         //     System.out.print(arr[i] + " ");
         // }
+        
         int j = 0;
         for(int i = 0; i < arr.length; i++){
             if(arr[i] != 0){
